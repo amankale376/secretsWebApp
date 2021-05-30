72aa545772ca59e14bcfeac747b617174f4f3ea2
Aman Kale <amankale.1998@gmail.com>
2021-05-30 17:47:12 +0530

    fist level security- password and email submittion in db

---
+++ app.js
@@ -0,0 +1,69 @@
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://localhost:27017/secrets", { useNewUrlParser: true,  useUnifiedTopology: true });
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = mongoose.model("user",userSchema);
+
+app.get("/", (req,res)=>{
+    res.render("home");
+});
+
+// /login
+app.route("/login")
+.get((req,res)=>{
+    res.render("login");
+})
+.post((req,res)=>{
+User.findOne({email:req.body.username}, (err, data)=>{
+    if(err){
+        res.send(err);
+    }else{
+        if(data){
+            if(data.password === req.body.password){
+                res.render("secrets");
+            }
+        }
+    }
+})
+});
+
+
+// /register
+app.route("/register")
+.get( (req,res)=>{
+    res.render("register");
+})
+.post((req,res)=>{
+  const user = new User({
+      email: req.body.username,
+      password:req.body.password
+  });
+  user.save((err)=>{ 
+      if(err){console.log(err);
+    }else{
+        console.log("user inserted");
+        res.render("secrets");
+    }
+  });  
+});
+
+
+
+app.listen(300, ()=>{
+    console.log("server has started at 300");
+});
