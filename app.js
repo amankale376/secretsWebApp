28ca0e5d4b3739e885d3ff8a59f9eb76725db3c8
Aman Kale <amankale.1998@gmail.com>
2021-05-30 19:47:38 +0530

    env and gitignore to keep safe confidentials

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
@@ -6,6 +7,8 @@ const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption')
 
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -18,8 +21,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "thisisoursecret";
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
+ //use environment variables and do not include them in git repository
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = mongoose.model("user",userSchema);
 
