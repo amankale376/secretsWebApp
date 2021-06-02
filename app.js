7cd0db0c391c12a6983f62eba03f9db31e44a863
Aman Kale <amankale.1998@gmail.com>
2021-06-02 15:32:24 +0530

    using Oauth for social website integration

--- app.js
+++ app.js
@@ -7,9 +7,11 @@ const mongoose = require('mongoose');
 // bellow packages are used for authentication and session creation
 // Warning!!! use them in same order
 const session = require('express-session');
-const passport = require('passport');
+const passport = require('passport'); //npm passport-local aswell but it doesnt need to require 
 const passportLocalMongoose = require('passport-local-mongoose');
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate'); //to make the google find and create a document in monoogse we use this predefined package
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 
 const app = express();
@@ -39,17 +41,57 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId:String
 });
 //using passportLocalmongoose plugin to make hash and salting 
 userSchema.plugin(passportLocalMongoose);
 
+// using findOrCreate plugin
+userSchema.plugin(findOrCreate);
+
 const User = mongoose.model("user",userSchema);
 
 passport.use(User.createStrategy()); // to create local login stratergy
-passport.serializeUser(User.serializeUser());  // creation of cookie than contains user information
-passport.deserializeUser(User.deserializeUser());   //breaking cookie to retrieve info for login 
+// passport.serializeUser(User.serializeUser());  // creation of cookie than contains user information
+// passport.deserializeUser(User.deserializeUser());   //breaking cookie to retrieve info for login 
+ passport.serializeUser(function(user, done) {
+     done(null, user.id);
+   });
+  
+   passport.deserializeUser(function(id, done) {
+     User.findById(id, function(err, user) {
+       done(err, user);
+     });
+   });
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:300/auth/google/secrets",
+     userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo" //google+ is deprecated thats why we need this code
+  },
+
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id}, function (err, user) { 
+        // you need to install "npm i mongoose-findorcreate" to make this code work
+      return cb(err, user);
+    });
+  }
+));
+
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: "http://localhost:300/auth/facebook/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 app.get("/", (req,res)=>{
     res.render("home");
 });
@@ -110,6 +152,31 @@ app.route("/secrets")
         res.redirect("/login");
     }
 });
+
+// /auth/google
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ["profile"] }));
+
+  app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    console.log("scuccessfully logged in");
+    res.redirect('/secrets');
+  
+  });
+
+  //auth facebook
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', {failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
 
 app.listen(300, ()=>{
     console.log("server has started at 300");
